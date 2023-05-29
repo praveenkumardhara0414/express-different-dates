de979799ed0609b8d15d40f61b0a67e4df780003
praveenkumar <praveendhara555@gmail.com>
2023-05-29 14:53:39 +0000

    export different dates form express

---
+++ app.js
@@ -0,0 +1,16 @@
+const addDays = require("date-fns/addDays");
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  const date = new Date();
+  const result = addDays(
+    new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+    100
+  );
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+});
+
+module.exports = app;
